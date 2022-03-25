4bd69e8acba9a9a15003711f1f9219905eae6807
WilliamGrossKC <wgross769@gmail.com>
2022-03-24 21:42:49 -0500

    Update app.cpp

--- proj4/app.cpp
+++ proj4/app.cpp
@@ -23,8 +23,8 @@ void get_hash_cb(rlbox_sandbox<rlbox_noop_sandbox>& _,
 void on_completion(rlbox::rlbox_sandbox<rlbox::rlbox_wasm2c_sandbox>& _,
                     rlbox::tainted<char*,rlbox::rlbox_wasm2c_sandbox> tainted_str) {
   
-    rlbox::rlbox_wasm2c_sandbox> result_str[100];
-    strcpy(result_str, tainted_str);
+    char result_str[100];
+    strcpy(result_str, tainted_str.UNSAFE_unverified());
     printf("Done: %s\n", result_str);
   
 }
