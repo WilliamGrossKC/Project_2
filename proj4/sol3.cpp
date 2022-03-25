9c7eaa64b93f454bbfdbb73fed3723fcf9fbd4ef
Denis Ivanov <denis.ivanov26@gmail.com>
2022-03-24 22:10:29 -0500

    Update sol3.cpp

--- proj4/sol3.cpp
+++ proj4/sol3.cpp
@@ -7,19 +7,29 @@
 #include "rlbox_wasm2c_sandbox.hpp"
 #include "rlbox.hpp"
 
-
-
+using namespace rlbox;
 //Callback on completion of library function
+
+/*
+void get_hash_cb(rlbox_sandbox<rlbox_noop_sandbox>& _,
+              tainted<const char*, rlbox_noop_sandbox> str) {
+  auto checked_string =
+    str.copy_and_verify_string([](std::unique_ptr<char[]> val) {
+        return std::strlen(val.get()) < 1024 ? std::move(val) : nullptr;
+    });
+  printf("hello_cb: %s\n", checked_string.get());
+}
+*/
+
 void on_completion(rlbox::rlbox_sandbox<rlbox::rlbox_wasm2c_sandbox>& _,
                     rlbox::tainted<char*,rlbox::rlbox_wasm2c_sandbox> tainted_str) {
-    
-    
-    
+  
     char result_str[100];
-    strcpy(result_str, result);
+    strcpy(result_str, tainted_str.UNSAFE_unverified());
     printf("Done: %s\n", result_str);
-    
+  
 }
+
 
 int main(int argc, char const *argv[])
 {
@@ -28,13 +38,27 @@ int main(int argc, char const *argv[])
         std::cerr << "Usage: " << argv[0] << " COPY_STRING RESULT_STRING" << std::endl;
         return 1;
     }
-
+    
+    rlbox::rlbox_sandbox<rlbox::rlbox_wasm2c_sandbox> sandbox;
+    sandbox.create_sandbox("./my_lib.so");
+    
     char* copy_str = (char*)argv[1];
+    size_t copySize = strlen(copy_str) + 1;
+    auto taintedStr1 = sandbox.malloc_in_sandbox<char>(copySize);
+    std::strcpy(taintedStr1.UNSAFE_unverified(), copy_str);
+            
     char* result_str = (char*)argv[2];
+    size_t resultSize = strlen(result_str) + 1;
+    auto taintedStr2 = sandbox.malloc_in_sandbox<char>(resultSize);
+    std::strcpy(taintedStr2.UNSAFE_unverified(), result_str);
+  
+    sandbox.invoke_sandbox_function(print_version);
+    auto cb = sandbox.register_callback(on_completion);
+    auto hash = sandbox.invoke_sandbox_function(get_hash,taintedStr1, cb, taintedStr2);
+    long long hash2 = hash.UNSAFE_unverified();
+    printf("Hash = %llx\n", hash2);
 
-    print_version();    
-    long long hash = get_hash(copy_str, on_completion, result_str);
-    printf("Hash = %llx\n", hash);
-
+    sandbox.destroy_sandbox();
     return 0;
 }
+
