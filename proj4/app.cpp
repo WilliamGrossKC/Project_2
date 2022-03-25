fe09c04ccc990cbde409c4449816009a81066c04
Denis Ivanov <denis.ivanov26@gmail.com>
2022-03-24 20:48:43 -0500

    Update app.cpp

--- proj4/app.cpp
+++ proj4/app.cpp
@@ -45,7 +45,7 @@ int main(int argc, char const *argv[])
     size_t resultSize = strlen(result_str) + 1;
     auto taintedStr2 = sandbox.malloc_in_sandbox<char>(resultSize);
     
-    sandbox.invoke_sandbox_function(printversion);
+    sandbox.invoke_sandbox_function(print_version);
     print_version();    
     long long hash = get_hash(copy_str, on_completion, result_str);
     printf("Hash = %llx\n", hash);
