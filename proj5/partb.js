c2396de643bc0548e68142553fcf5d2fb3b66de7
WilliamGrossKC <wgross769@gmail.com>
2022-04-09 22:45:44 -0500

    Update partb.js

--- proj5/partb.js
+++ proj5/partb.js
@@ -1,7 +1,7 @@
 /*
  * TODO: Part B solution goes here!
  */
-let dataplane = new Uint32Array(1);
+let data = new Uint32Array(1);
 let strfront = String.fromCharCode(1, 0, 0, 0, 0, 225, 245, 5, 0, 0, 0, 0, 0, 0, 0, 0);
 let fakestring = cs361s.fakestr(cs361s.addrof(strfront) + 16);
 
@@ -19,11 +19,10 @@ while(index < codes.length){
     index++;
 }
 
-
 var fb = 0;
 while(fb < 8) {
     var bit = 0;
-    let temp = cs361s.addrof(dataplane);
+    let temp = cs361s.addrof(data);
     while(bit < fb) {
         temp = (temp - codes[72 - 16 + bit])/256;
         bit++;
@@ -32,43 +31,22 @@ while(fb < 8) {
     fb++;
 }
 
-codes[0] = codes[0] + 1;
+// codes[0] = codes[0] + 1;
 let cc = String.fromCharCode(...codes)
-let controlPlane = cs361s.fakeobj(cs361s.addrof(cc) + 16);
-
+let control = cs361s.fakeobj(cs361s.addrof(cc) + 16);
 
 let editmem = function(addr) {
     let num = Math.pow(2, 32);
-    controlPlane[14] = addr % num;
-    controlPlane[15] = ((addr - controlPlane[0]) / num);
+    control[14] = addr % num;
+    control[15] = ((addr - control[0]) / num);
 };
 
 let readmem = function(addr) {
     editmem(addr);
-    return dataplane[0];
+    return data[0];
 };
 
 let writemem = function(addr, val) {
     editmem(addr);
-    dataplane[0] = val;
+    data[0] = val;
 };
-
-
-
-/*
-// create the control plane
-codes[0] = codes[0] + 1;
-let controlPlane = cs361s.fakeobj(cs361s.addrof(String.fromCharCode(...codes)) + 16);
-
-let readmem = function(addr) {
-    controlPlane[14] = addr % (Math.pow(2, 32));
-    controlPlane[15] = (addr - controlPlane[0]) / (Math.pow(2, 32));
-    return dataplane[0];
-};
-
-let writemem = function(addr, val) {
-    controlPlane[14] = addr % (Math.pow(2, 32));
-    controlPlane[15] = (addr - controlPlane[0]) / (Math.pow(2, 32));
-    dataplane[0] = val;
-};
-*/
