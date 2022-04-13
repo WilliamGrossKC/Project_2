b33e467be4ae4f3b8e8a4aecaada60eca16714aa
Denis Ivanov <denis.ivanov26@gmail.com>
2022-04-12 23:41:44 -0500

    Update partc.js

--- proj5/partc.js
+++ proj5/partc.js
@@ -12,7 +12,7 @@ const ctx = cs361s.readmem(cs361s.addrof(console.log) + 48);
 const rt = cs361s.readmem(ctx + 24);
 const jsMallocUsableSize = cs361s.readmem(rt + 24);
 const auipc = cs361s.readmem(jsMallocUsableSize) >>> 12 << 12; // 20 immediate bits
-console.log(auipc);
+console.log((auipc >>> 0).toString(2));
 const load = cs361s.readmem(jsMallocUsableSize + 4) >> 20; // 12 immediate bits
 //GOT = game of thrones :)
 const gameofthrones = cs361s.readmem(jsMallocUsableSize + auipc + load);
