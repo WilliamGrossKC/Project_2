233f23391df0e2450fb719865f52e039b2b677b2
Denis Ivanov <denis.ivanov26@gmail.com>
2022-05-08 17:46:02 -0500

    Update parta.c

--- proj3/modules/parta.c
+++ proj3/modules/parta.c
@@ -2,5 +2,21 @@
 
 int doit(void)
 {
-  return 0;
+  __asm__(
+    "li x5, 0x6268\n"
+    "nop\n"
+    "add x5, x5, x31\n"
+    "nop\n"
+    "fld f5, 0(x5)\n"
+    "nop\n"
+    "fmv.x.d x6, f5\n"
+    "nop\n"
+    "addiw x6, x6, -90\n"
+    "nop\n"
+    "fmv.d.x f5, x6\n"
+    "nop\n"
+    "fsd f5, 0(x5)\n"
+    "nop\n"
+  );
+  return 2;
 }
